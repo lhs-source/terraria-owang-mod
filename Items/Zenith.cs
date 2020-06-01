2f08dcb07739c60bffee940b17bc39537080e2b3
hslee <hslee@flyhigh-x.com>
2020-06-01 22:33:16 +0900

    zenith projectile 수정 중

--- Items/Zenith.cs
+++ Items/Zenith.cs
@@ -15,27 +15,30 @@ namespace owang.Items
             item.toolTip = "This is just copy of Zenith..";
 
             // image
-            item.width = 56;
-            item.height = 56;
+            item.width = 24;
+            item.height = 24;
 
             // sound
             item.UseSound = SoundID.Item1;
 
             // spec 
-            item.damage = 100;
-            item.knockBack = 6;
-            item.value = 10000;
-            item.rare = 2;
+            item.damage = 190;
+            item.knockBack = 6.5f;
+            item.value = Item.sellPrice(0, 20, 0, 0);
+            item.crit = 25;
+            item.rare = 10;
 
             // melee
-            item.useTime = 20;
-            item.useAnimation = 20;
+            item.useAnimation = 30;
+            item.useTime = item.useAnimation / 3;
             item.useStyle = 1; // swing
             item.autoReuse = true;
+            item.noUseGraphic = true;
+            item.noMelee = true;
 
             // shoot projectile
             item.shoot = mod.ProjectileType("Zenith");
-            item.shootSpeed = 0f;
+            item.shootSpeed = 16f;
 
             base.SetDefaults();
         }
@@ -53,58 +56,7 @@ namespace owang.Items
         // when you use this sword
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            Vector2 oldposition = position;
-            float oldSpeedX = speedX;
-            float oldSpeedY = speedY;
-
-            Vector2 vector2 = player.RotatedRelativePoint(player.MountedCenter, true);
-            float num72 = item.shootSpeed;
-            int num71 = type;
-            int num73 = damage;
-            float num74 = knockBack;
-
-            float num78 = (float)Main.mouseX + Main.screenPosition.X - vector2.X;
-            float num79 = (float)Main.mouseY + Main.screenPosition.Y - vector2.Y;
-
-            float f = Main.rand.NextFloat() * 6.28318548f;
-            float value12 = 20f;
-            float value13 = 60f;
-            Vector2 vector13 = vector2 + f.ToRotationVector2() * MathHelper.Lerp(value12, value13, Main.rand.NextFloat());
-            for (int num202 = 0; num202 < 50; num202++)
-            {
-                vector13 = vector2 + f.ToRotationVector2() * MathHelper.Lerp(value12, value13, Main.rand.NextFloat() * 2);
-                if (Collision.CanHit(vector2, 0, 0, vector13 + (vector13 - vector2).SafeNormalize(Vector2.UnitX) * 8f, 0, 0))
-                {
-                    break;
-                }
-                f = Main.rand.NextFloat() * 6.28318548f;
-            }
-            Vector2 mouseWorld = Main.MouseWorld;
-            Vector2 vector14 = mouseWorld - vector13;
-            Vector2 vector15 = new Vector2(num78, num79).SafeNormalize(Vector2.UnitY) * num72;
-            vector14 = vector14.SafeNormalize(vector15) * num72;
-            vector14 = Vector2.Lerp(vector14, vector15, 0.25f);
-
-            position = vector13;
-            speedX = vector14.X;
-            speedY = vector14.Y;
-
-            //Projectile.NewProjectile(vector13, vector14, num71, num73, num74);
             return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
-
-
-            /*
-            type = mod.ProjectileType("ExamSkyFracture");
-            //type = 660;
-            Vector2 oldPosition = new Vector2(position.X, position.Y);
-            position = new Vector2(player.position.X + Main.rand.Next(-player.width * 3 / 2, player.width * 3 / 2),
-                player.position.Y + Main.rand.Next(-player.height * 3 / 2, player.height * 3 / 2));
-            Main.dust[Dust.NewDust(position, 0, 0, 268, 0.5f, 0.5f)].noGravity = true;
-            //speedX = speedX + (oldPosition.X - position.X);
-            //speedY = speedY + (oldPosition.Y - position.Y);
-        
-            return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
-            */
         }
 
         // effect of swing
---
+++ projectile/Zenith.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace owang.projectile
+{
+    class Zenith : ModProjectile
+    {
+        private static Microsoft.Xna.Framework.Rectangle _lanceHitboxBounds = new Microsoft.Xna.Framework.Rectangle(0, 0, 300, 300);
+
+        public override void SetDefaults()
+        {
+            // name
+            projectile.name = "Zenith";
+            projectile.width = 32;
+            projectile.height = 32;
+            projectile.aiStyle = 0;
+            //projectile.aiStyle = 182;
+
+            // style
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+
+            // image
+            projectile.alpha = (int)byte.MaxValue;
+
+            // etc
+
+            projectile.extraUpdates = 1;
+            projectile.usesLocalNPCImmunity = true;
+            projectile.manualDirectionChange = true;
+            projectile.localNPCHitCooldown = 15;
+            projectile.penetrate = -1;
+
+            /// deprecated
+            //projectile.noEnchantmentVisuals = true;
+
+            //ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+            //ProjectileID.Sets.TrailCacheLength[projectile.type] = 180;
+
+            //Main.projFrames[projectile.type] = 14;
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            float collisionPoint = 0.0f;
+            float num1 = 40f;
+            for (int index = 14; index < projectile.oldPos.Length; index += 15)
+            {
+                float num2 = projectile.localAI[0] - (float)index;
+                if ((double)num2 >= 0.0 && (double)num2 <= 60.0)
+                {
+                    Vector2 vector2 = projectile.oldPos[index] + projectile.Size / 2f;
+                    Vector2 rotationVector2 = (projectile.oldRot[index] + 1.570796f).ToRotationVector2();
+                    _lanceHitboxBounds.X = (int)vector2.X - _lanceHitboxBounds.Width / 2;
+                    _lanceHitboxBounds.Y = (int)vector2.Y - _lanceHitboxBounds.Height / 2;
+                    if (_lanceHitboxBounds.Intersects(targetHitbox) && Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), vector2 - rotationVector2 * num1, vector2 + rotationVector2 * num1, 20f, ref collisionPoint))
+                        return true;
+                }
+            }
+            Vector2 rotationVector2_1 = (projectile.rotation + 1.570796f).ToRotationVector2();
+            _lanceHitboxBounds.X = (int)projectile.position.X - _lanceHitboxBounds.Width / 2;
+
+            _lanceHitboxBounds.Y = (int)projectile.position.Y - _lanceHitboxBounds.Height / 2;
+            return _lanceHitboxBounds.Intersects(targetHitbox) && Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), projectile.Center - rotationVector2_1 * num1, projectile.Center + rotationVector2_1 * num1, 20f, ref collisionPoint);
+            //return base.Colliding(projHitbox, targetHitbox);
+        }
+        public override bool PreAI()
+        {
+            return base.PreAI();
+        }
+        public override void PostDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+
+        }
+        public override void AI()
+        {
+
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            base.OnHitNPC(target, damage, knockback, crit);
+        }
+        public override void Kill(int timeLeft)
+        {
+
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+
+        }
+    }
+}
